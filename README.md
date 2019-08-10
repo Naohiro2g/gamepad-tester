5527a33bb907bbbbe77b66cdc01d2a630bdca12c
Naohiro Tsuji <naohiro2g@code2create.club>
2019-08-11 04:02:09 +0900

    axis and hat values for sensor-update.  python3 compatible.

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # gamepad-tester
 to use gamepads in Python with pygame and pygame zero, or in Scratch
 
+## How to use
 
-# references
+1. Connect your gamepad to your computer via USB. Gamepad for PS: Play Station or SNES, or SNES: Super Nintendo Entertainment System can be used.
+2. Launch Scratch 1.4, then open gamepad_hat_axis.sb to make it server for Remote Sensors Protocol.
+3. Launch Python3 code 'test-scratch.py' by  ```python3 test-scratch.py``` in the LXTerminal window.
+4. New window pops up for the status of your gamepad connected to your computer via USB.
+5. You can use youe gamepad to control the Cat, Bat, and Butterfly.
+
+Python3 compatible Scratch-RSP test code is [here](https://github.com/Naohiro2g/rsp-test).
+
+
+# Reference to Scratch Remote Sensors Protovol
+
+Newer versions of Scratch-RSP test code in Python3
+https://github.com/Naohiro2g/rsp-test
+
 Remote Sensors Protocol (Scratch 1.4)
 https://en.scratch-wiki.info/wiki/Remote_Sensors_Protocol
 
@@ -11,4 +25,3 @@ https://en.scratch-wiki.info/wiki/Communicating_to_Scratch_via_Python_with_a_GUI
 
 Code in Python 3
 https://en.scratch-wiki.info/wiki/Communicating_to_Scratch_via_Python
-
--- test-scratch.py
+++ test-scratch.py
@@ -9,29 +9,40 @@ from array import array
 import socket
 import time
 import sys
-from Tkinter import Tk
-from tkSimpleDialog import askstring
+import struct
+import unicodedata
 
-root = Tk()
-root.withdraw()
+PORT = 42001         # Scratch Remote Sensors Protocol port
+HOST = 'localhost'   # his-raspi.local or 192.168.1.22
+                     # to connect to another computer on the same network
 
-PORT = 42001
-HOST = askstring('Scratch Connector', 'IP:')
-if not HOST: HOST = 'localhost'
-
-print("connecting...")
+print("\n", "Connecting...")
 scratchSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 scratchSock.connect((HOST, PORT))
-print("connected")
+print("Connected to Scratch!")
+print('\n', 'Gamepad tester started...\n')
+
 
 def sendScratchCommand(cmd):
-    n = len(cmd)
-    a = array('c')
-    a.append(chr((n >> 24) & 0xFF))
-    a.append(chr((n >> 16) & 0xFF))
-    a.append(chr((n >>  8) & 0xFF))
-    a.append(chr(n & 0xFF))
-    scratchSock.send(a.tostring() + cmd)
+    scratchSock.send(struct.pack(">I",lenCount(cmd)))
+    print(struct.pack(">I",lenCount(cmd)))
+    scratchSock.send(bytes(cmd, 'UTF-8'))
+    print(bytes(cmd, 'UTF-8'))
+
+def lenCount(text):
+    count = 0
+    for c in text:
+        if unicodedata.east_asian_width(c) in 'FWA':
+            count += 3
+        else:
+            count += 1
+    return count
+
+def send_broadcast(msg):
+    sendScratchCommand('broadcast "' + msg + '"')
+
+def send_sensor_update(sensor_name, sensor_value):
+    sendScratchCommand('sensor-update "' + sensor_name + '" ' + sensor_value + ' ')
 
 
 # Define some colors
@@ -50,59 +61,97 @@ class TextPrint:
         textBitmap = self.font.render(textString, True, BLACK)
         screen.blit(textBitmap, [self.x, self.y])
         self.y += self.line_height
-        
+
     def reset(self):
         self.x = 10
         self.y = 10
         self.line_height = 15
-        
+
     def indent(self):
         self.x += 10
-        
+
     def unindent(self):
         self.x -= 10
-    
+
 pygame.init()
 
 # Set the width and height of the screen [width,height]
-size = [500, 700]
+size = [500, 800]
 screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("My Game")
-
+pygame.display.set_caption("Gamepad Tester")
 #Loop until the user clicks the close button.
 done = False
-
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-
 # Initialize the joysticks
 pygame.joystick.init()
-    
 # Get ready to print
 textPrint = TextPrint()
 
+# Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+JOYSTICK_ACTIONS = (
+    pygame.JOYAXISMOTION,
+    pygame.JOYBUTTONDOWN,
+    pygame.JOYBUTTONUP,
+    pygame.JOYHATMOTION
+)
+
+
+def send_joystick(event):
+    if event.type == pygame.JOYBUTTONDOWN:
+        msg = str(event.joy) + "_" + "Button_" + str(event.button) + "_on"
+        send_broadcast(msg)
+    if event.type == pygame.JOYBUTTONUP:
+        msg = str(event.joy) + "_" + "Button_" + str(event.button) + "_off"
+        send_broadcast(msg)
+    if event.type == pygame.JOYAXISMOTION:
+        sensor_name = str(event.joy) + "_" + "Axis_" + str(event.axis)
+        sensor_value = axis_value(event.value)
+#        send_broadcast(sensor_name)
+        send_sensor_update(sensor_name, sensor_value)
+    if event.type == pygame.JOYHATMOTION:
+#        msg = str(event.joy) + "_" + "Hat_" + str(event.value)
+#        msg = str(event.joy) + "_" + "Hat"
+#        send_broadcast(msg)
+        value = str(event.value)
+        if value[1] == "-":
+            hat_x = "-1"
+        else:
+            hat_x = value[1]
+        hat_y = str(event.value)[-3:-1]
+        sensor_name = str(event.joy) + "_" + "Hat_X"
+        sensor_value = hat_x
+        send_sensor_update(sensor_name, sensor_value)
+        sensor_name = str(event.joy) + "_" + "Hat_Y"
+        sensor_value = hat_y
+        send_sensor_update(sensor_name, sensor_value)
+
+
+from decimal import Decimal, ROUND_HALF_UP
+
+def axis_value(value):
+    if value > 0.9:
+        value = 1.0
+    elif 0.1 > value > -0.1:
+        value = 0.0
+    elif value < -0.9:
+        value = -1.0
+    value = Decimal(str(value)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
+    return(str(value))
+
+
+
 # -------- Main Program Loop -----------
 while done==False:
+
     # EVENT PROCESSING STEP
     for event in pygame.event.get(): # User did something
         now = datetime.datetime.now()
         if event.type == pygame.QUIT: # If user clicked close
             done=True # Flag that we are done so we exit this loop
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN:
-            msg = "Button_" + str(event.button) + "_on" 
-            sendScratchCommand('broadcast "' + msg + '"')
-#            printS("Button:" + str(event.button) + " pressed.   "  + now.strftime("  %H:%M:%S.") + "%03d" % (now.microsecond // 1000))
-        if event.type == pygame.JOYBUTTONUP:
-            msg = "Button_" + str(event.button) + "_off" 
-            sendScratchCommand('broadcast "' + msg + '"')
-#            printS("Button:" + str(event.button) + " released.  "  + now.strftime("  %H:%M:%S.") + "%03d" % (now.microsecond // 1000))
-        if event.type == pygame.JOYAXISMOTION:
-            msg = "Axis_" + str(event.axis)
-            sendScratchCommand('broadcast "' + msg + '"')
-#            printS("Axis:{} {:>6.3f} ".format(event.axis, event.value) + now.strftime("  %H:%M:%S.") + "%03d" % (now.microsecond // 1000) )
- 
+        if event.type in JOYSTICK_ACTIONS:
+            send_joystick(event)
+
     # DRAWING STEP
     # First, clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
@@ -111,34 +160,32 @@ while done==False:
 
     # Get count of joysticks
     joystick_count = pygame.joystick.get_count()
-
     textPrint.printS(screen, "Number of joysticks: {}".format(joystick_count) )
     textPrint.indent()
-
 
     # For each joystick:
     for i in range(joystick_count):
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
-    
+
         textPrint.printS(screen, "Joystick {}".format(i) )
         textPrint.indent()
-    
+
         # Get the name from the OS for the controller/joystick
         name = joystick.get_name()
         textPrint.printS(screen, "Joystick name: {}".format(name) )
-        
+
         # Usually axis run in pairs, up/down for one, and left/right for
         # the other.
         axes = joystick.get_numaxes()
         textPrint.printS(screen, "Number of axes: {}".format(axes) )
         textPrint.indent()
-        
+
         for i in range( axes ):
             axis = joystick.get_axis( i )
             textPrint.printS(screen, "Axis {} value: {:>6.7f}".format(i, axis) )
         textPrint.unindent()
-            
+
         buttons = joystick.get_numbuttons()
         textPrint.printS(screen, "Number of buttons: {}".format(buttons) )
         textPrint.indent()
@@ -147,7 +194,7 @@ while done==False:
             button = joystick.get_button( i )
             textPrint.printS(screen, "Button {:>2} value: {}".format(i,button) )
         textPrint.unindent()
-            
+
         # Hat switch. All or nothing for direction, not like joysticks.
         # Value comes back in an array.
         hats = joystick.get_numhats()
@@ -158,15 +205,14 @@ while done==False:
             hat = joystick.get_hat( i )
             textPrint.printS(screen, "Hat {} value: {}".format(i, str(hat)) )
         textPrint.unindent()
-        
+
     # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-    
+
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
-
     # Limit to 20 frames per second
     clock.tick(50)
-    
+
 # Close the window and quit.
 # If you forget this line, the program will 'hang'
 # on exit if running from IDLE.
@@ -174,5 +220,5 @@ pygame.quit ()
 
 print("closing socket...")
 scratchSock.close()
-print("done")
+print("\n", "bye...")
 sys.exit()
